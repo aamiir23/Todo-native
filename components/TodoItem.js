0e17013b3fc9dac5ff9bc95b03bf86b06b5d857f
aamiir23 <iammdaamiralam@gmail.com>
2024-09-24 23:12:54 +0530

    add changes for completion

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -3,13 +3,21 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import '../styles';
 import styles from '../styles';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+
 const TodoItem = ({ todo, onToggleComplete, onDelete }) => {
   return (
     <View>
 
     <TouchableOpacity onPress={() => onToggleComplete(todo.id)}>
       <View  style={styles.todoItem}>
-        <Text style={{ textDecoration: todo.completed ? 'line-through' : 'line-through', fontSize:14, textAlign:'center', color:'#1d3557' }}>
+      <Text style={{
+        textDecorationLine: todo.completed ? 'line-through' : 'none',
+        textDecorationStyle: todo.completed ? 'solid' : 'none',
+        textDecorationColor: todo.completed ? '#111111' : 'none',
+        fontSize: 14,
+        textAlign: 'center',
+        // color: '#fff00f',
+        }}>
           {todo.text}
         </Text>
         <TouchableOpacity onPress={() => onDelete(todo.id)}>
